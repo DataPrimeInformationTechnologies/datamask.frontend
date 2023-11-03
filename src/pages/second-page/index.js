f0ffa2a1da77a448c305069345676ba6a8c6e125
tuncay.yakar <yakartuncay@gmail.com>
2023-11-03 16:09:45 +0300

    Configuration page

--- src/pages/second-page/index.js
+++ src/pages/second-page/index.js
@@ -4,19 +4,41 @@ import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { useState } from 'react'
 
 const SecondPage = () => {
+  const [dbType, setDbType] = useState("postgresql");
+
+  const handleChange = (event) => {
+    setDbType(event.target.value);
+  };
+
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
         <Card>
-          <CardHeader title='Create Awesome 🙌'></CardHeader>
+          <CardHeader title='Create Database Configuration'></CardHeader>
           <CardContent>
-            <Typography sx={{ mb: 2 }}>This is your second page.</Typography>
+          <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">DB Type</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={dbType}
+          label="DB Type"
+          onChange={handleChange}
+        >
+          <MenuItem value="postgresql">PostgreSQL</MenuItem>
+          <MenuItem value="oracle">Oracle</MenuItem>
+          <MenuItem value="mssql">MS SQL</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+            <Typography sx={{ mb: 2 }}>Please Select Database Type</Typography>
             <Typography>
-              Chocolate sesame snaps pie carrot cake pastry pie lollipop muffin.
-              Carrot cake dragée chupa chups jujubes. Macaroon liquorice cookie
-              wafer tart marzipan bonbon. Gingerbread jelly-o dragée chocolate.
+              In progress
             </Typography>
           </CardContent>
         </Card>
